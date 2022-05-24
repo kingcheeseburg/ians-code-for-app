e0733e9b336a780b03fb7666f5816e99aaf215a3
kingcheeseburg <105747197+kingcheeseburg@users.noreply.github.com>
2022-05-24 13:39:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ root.geometry('400x300')
 root['bg'] = 'blue'
 
 frame1 = Frame(root, width=200, height=300, bg="red")
-frame1.pack(side="right")
+frame1.config(width=200, height=150)
+frame1.pack(side=RIGHT, fill='both', expand=True)
+
+frame2 = Frame(root, bg="orange")
+frame2.config(width=200, height=150)
+frame2.pack(side=LEFT, fill='both', expand=True)
 
 def printValue():
     post = Post.get()
-    Label(frame1, text=f'{post}', bg='red').pack()
+    Label(frame1, text=f'{post}', bg='red').pack(side=TOP)
 
-Post = Entry(root)
-Post.pack(pady=30)
+Post = Entry(frame2)
+Post.pack(side=TOP)
 
-Button(root, text="Submit Post", padx=10, pady=5, command=printValue).pack()
+Button1 = Button(frame2, text="Submit Post", command=printValue)
+Button1.pack(side=LEFT)
 
 root.mainloop()
